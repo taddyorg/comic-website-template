47a73c2f26a7471f0f3702fbad6b2ee681392a21
Glitch (glitch-hello-react) <none>
2023-12-31 18:09:32 +0000

    🚴🎀 Checkpoint ./src/components/router.jsx:63946637/1064

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -4,16 +4,16 @@ import Home from "../pages/home";
 import About from "../pages/about";
 
 /**
-* The router is imported in app.jsx
-*
-* Our site just has two routes in it–Home and About
-* Each one is defined as a component in /pages
-* We use Switch to only render one route at a time https://github.com/molefrog/wouter#switch-
-*/
+ * The router is imported in app.jsx
+ *
+ * Our site just has two routes in it–Home and About
+ * Each one is defined as a component in /pages
+ * We use Switch to only render one route at a time https://github.com/molefrog/wouter#switch-
+ */
 
 export default () => (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
+  <Switch>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </Switch>
 );
