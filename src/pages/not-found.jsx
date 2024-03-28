6ba85e9b4530ffc223f36aa6fb4204958a64e512
Daniel Mathews <dannyl.mathews@gmail.com>
2024-03-27 22:57:22 -0700

    update 404 page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 export default function NotFound() {  
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="flex justify-center items-center flex-col">
+      <h1 className='text-lg font-bold mt-2 text-primary'>404 - Not Found</h1>
+      <p className='text-lg font-bold mt-2 text-primary'>The page you are looking for does not exist.</p>
     </div>
   );
 }
